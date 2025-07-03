4719aceb7838199237cd74ecb15e55200cda7d48
Christian Will <christian.will@bridging-it.de>
2025-07-03 08:56:57 +0200

    Added regions

--- 10-initial-project/src/Program.cs
+++ 10-initial-project/src/Program.cs
@@ -1,16 +1,20 @@
 
+#region using Directives
 using Microsoft.EntityFrameworkCore;
 using Data;
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.Extensions.Logging;
 using System.Text.RegularExpressions;
+#endregion
 
 Console.WriteLine("BlogSystem Console Demo gestartet.");
 
+#region Database Setup
 int sqlCount = 0;
 var options = new DbContextOptionsBuilder<AppDbContext>()
     .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BlogSystemDb;Trusted_Connection=True;")
-    //.UseLazyLoadingProxies() // Aktiviert Lazy Loading
+    // .UseLazyLoadingProxies() // Aktiviert Lazy Loading
     .LogTo(log =>
     {
         Console.WriteLine(log);
@@ -30,6 +34,8 @@ db.Database.EnsureCreated();
 // Seed initial ausführen, falls leer
 // DbSeeder.Seed(db);
 
+#endregion
+
 // Endlosschleife zum Testen
 while (true)
 {
@@ -40,18 +46,32 @@ while (true)
     var stopwatch = Stopwatch.StartNew();
 
     var blogs = db.Blogs
+        .Include(b => b.Posts)
+        .ThenInclude(p => p.Comments)
         .ToList();
 
     stopwatch.Stop();
 
     foreach (var blog in blogs)
     {
-        Console.WriteLine($"> {blog.Name} ({blog.Posts.Count} Posts)");
+        Console.WriteLine($"> {blog.Name}");
+        foreach (var post in blog.Posts)
+        {
+            Console.WriteLine($"{post.Title}");
+            foreach (var comment in post.Comments)
+            {
+                Console.WriteLine($"Author: {comment.Author}, Content: {comment.Content}, CreatedAt: {comment.CreatedAt}");
+            }
+        }
+
     }
 
+    #region Output
     Console.WriteLine();
-    Console.WriteLine($"Dauer: {stopwatch.ElapsedMilliseconds} ms");
+    Console.WriteLine($"Dauer: {stopwatch.Elapsed} ms");
     Console.WriteLine($"Anzahl SQL-Kommandos: {sqlCount}");
     Console.WriteLine("Drücke Enter für den nächsten Lauf...");
     Console.ReadLine();
+    #endregion    
+
 }
