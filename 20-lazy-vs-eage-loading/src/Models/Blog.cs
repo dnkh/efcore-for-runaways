14995085e3304d38f64f364606b06b3245c782ab
Christian Will <christian.will@dotnet-knowhow.de>
2025-07-03 08:34:49 +0000

    Inital project for lazy vs eager loading

---
+++ 20-lazy-vs-eage-loading/src/Models/Blog.cs
@@ -0,0 +1,10 @@
+namespace Models;
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+
+    public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+}
